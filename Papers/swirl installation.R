665b02cff70574021697b59ddcb174d0cea9a7a6
IggyZhao <59684539+IggyZhao@users.noreply.github.com>
2020-08-12 01:03:55 -0400

    Add files via upload

---
+++ Papers/swirl installation.R
@@ -0,0 +1,8 @@
+install.packages("swirl")
+library("swirl")
+getwd()
+install_course_zip("/Users/Evangeline/Desktop/MIT Data Analysis for Social Scientists/R Code/14_310x_Intro_to_R.zip",multi = FALSE)
+swirl()
+
+
+
